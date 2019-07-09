ba506305a3ff41e189a8cbd973fc50df3f3f1d94
Dauke Zuchanke <dz17278@bristol.ac.uk>
2019-07-09 11:47:21 +0100

    Added check for redundant sourcing in script.

--- myFullRunScript.sh
+++ myFullRunScript.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Does a basic CLIC simulation and reconstruction.
 
-source /cvmfs/clicdp.cern.ch/iLCSoft/builds/2019-04-17/x86_64-slc6-gcc62-opt/init_ilcsoft.sh
+
+# Checks to see if already sourced
+if [ -z "$ILCSOFT" ]
+then
+    echo Sourcing ILCSOFT
+    source /cvmfs/clicdp.cern.ch/iLCSoft/builds/2019-04-17/x86_64-slc6-gcc62-opt/init_ilcsoft.sh
+fi
 
 ddsim --steeringFile clic_steer.py --compactFile $ILCSOFT/lcgeo/HEAD/CLIC/compact/CLIC_o3_v14/CLIC_o3_v14.xml --enableGun --gun.particle mu- --gun.energy 10*GeV  --gun.distribution uniform --outputFile mySimOutputFile.slcio --numberOfEvents 100
 
