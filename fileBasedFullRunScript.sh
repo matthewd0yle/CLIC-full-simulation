3139306a2864b2e8daeca32ddac6e4e89345e369
Dauke Zuchanke <dz17278@bristol.ac.uk>
2019-09-12 16:24:23 +0100

    Fixed run script.

--- fileBasedFullRunScript.sh
+++ fileBasedFullRunScript.sh
@@ -9,6 +9,6 @@ then
     source /cvmfs/clicdp.cern.ch/iLCSoft/builds/2019-04-17/x86_64-slc6-gcc62-opt/init_ilcsoft.sh
 fi
 
-ddsim --steeringFile clic_steer.py --compactFile CLIC_o3_vCustom/CLIC_o3_v14.xml --inputFile whizard.001.stdhep --outputFile mySimOutputFile.slcio --numberOfEvents 100
+ddsim --compactFile CLIC_o3_vCustom/CLIC_o3_v14.xml --inputFile whizard.001.stdhep --outputFile mySimOutputFile.slcio --numberOfEvents 10
 
 Marlin --InitDD4hep.DD4hepXMLFile="CLIC_o3_vCustom/CLIC_o3_v14.xml" --global.LCIOInputFiles="mySimOutputFile.slcio"  myEditedClicReconstruction.xml
