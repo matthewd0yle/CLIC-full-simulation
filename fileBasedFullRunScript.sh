a40d80a99e2b390b3f1bb9ae632de67f37a33380
Dauke Zuchanke <dz17278@bristol.ac.uk>
2019-09-17 10:47:12 +0100

    Updated file based run script.

--- fileBasedFullRunScript.sh
+++ fileBasedFullRunScript.sh
@@ -6,7 +6,7 @@
 if [ -z "$ILCSOFT" ]
 then
     echo Sourcing ILCSOFT
-    source /cvmfs/clicdp.cern.ch/iLCSoft/builds/2019-04-17/x86_64-slc6-gcc62-opt/init_ilcsoft.sh
+    source /cvmfs/clicdp.cern.ch/iLCSoft/builds/2019-09-04/x86_64-slc6-gcc62-opt/init_ilcsoft.sh
 fi
 
 ddsim --compactFile CLIC_o3_vCustom/CLIC_o3_v14.xml --inputFile whizard.001.stdhep --outputFile mySimOutputFile.slcio --numberOfEvents 10
